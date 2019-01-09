d5f0fc2a8138354b4c05c142365f78705240b500
Xiaoping-Li <narci2008@gmail.com>
2019-01-09 14:20:34 -0800

    [SWDEV-120]Style About page our-story

--- react-ankr-web/src/components/about/About.js
+++ react-ankr-web/src/components/about/About.js
@@ -5,6 +5,8 @@ import bracket from '../../images/bracket.svg';
 import e7 from '../../images/e-7.svg';
 import e8 from '../../images/e-8.svg';
 import e9 from '../../images/e-9.svg';
+import ankrLogo from '../../images/logo-icon.svg';
+import signature from '../../images/signature.png';
 
 
 import Team from './Team';
@@ -61,23 +63,37 @@ class About extends Component {
 
                 {/**Second part of About page, named "story" */}
                 <div className="story" id="our-story">
-                    <h2>Our story</h2>
-                    <p>"I believe that cloud computing should be ubiquitous in the near future, but it’s currently monopolized 
-                        by tech giants. High margins are charged against users due to high infrastructure and human capital costs.</p>
-                    <p>There should be a way for researchers, students, and startups to access affordable cloud computing that
-                            isn’t centralized. Therefore, I founded Ankr together with Stanley Wu and Ryan Fang.</p>
-                    <p>Stanley was my mentor during my internship. As for Ryan, our mutual trust established gradually 
-                        throughout our college career.</p>
-                    <p>We were roommates for four years, and I witnessed how he progressed from an ambitious freshman to a 
-                        competent investment baking analyst.</p>
-                    <p>Our goal is to make this team a family and keep pushing our boundaries to realize our ultimate goal: 
-                        making the blockchain token model accessible, and decentralized cloud computing an everyday reality."</p>
-                    <div>
-                        <div>
-                            <div>Chandler Song</div>
-                            {/*Ankr Logo & Song's Title */}
+                    <div className="story-title">
+                        <h2>Our story</h2>
+                        <div className="story-item story-divider"></div>
+                    </div>
+
+                    <div className="story-body">
+                        <div className="story-left">
+                            <p>"I believe that cloud computing should be ubiquitous in the near future, but it’s currently monopolized
+                                by tech giants. High margins are charged against users due to high infrastructure and human capital costs.</p>
+                            <p>There should be a way for researchers, students, and startups to access affordable cloud computing that
+                                isn’t centralized. Therefore, I founded Ankr together with Stanley Wu and Ryan Fang.</p>
+                            <p>Stanley was my mentor during my internship. As for Ryan, our mutual trust established gradually
+                                throughout our college career.</p>
                         </div>
-                        <div className="signature-song">{/*<img src="" alt="chandler signature" /> */}</div>
+
+                        <div className="story-right">
+                            <p>We were roommates for four years, and I witnessed how he progressed from an ambitious freshman to a
+                                competent investment baking analyst.</p>
+                            <p>Our goal is to make this team a family and keep pushing our boundaries to realize our ultimate goal:
+                                making the blockchain token model accessible, and decentralized cloud computing an everyday reality."</p>
+                            <div className="story-right-sign">
+                                <div className="story-right-sign-left">
+                                    <h3>Chandler Song</h3>
+                                    <div className="story-right-sign-left-desc">
+                                        <img className="story-ankr-logo" src={ankrLogo} />
+                                        <div className="story-right-sign-left-title">CEO of Ankr Network</div>
+                                    </div>
+                                </div>
+                                <img className="signature-song" src={signature} />
+                            </div>
+                        </div>
                     </div>
                 </div>
 
